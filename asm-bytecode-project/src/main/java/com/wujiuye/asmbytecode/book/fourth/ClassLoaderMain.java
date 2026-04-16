564e99032b3cdd5365f57e95a05e53aadef7c827
suming <suming22x@163.com>
2026-04-16 21:48:47 +0800

    note: learn

--- asm-bytecode-project/src/main/java/com/wujiuye/asmbytecode/book/fourth/ClassLoaderMain.java
+++ asm-bytecode-project/src/main/java/com/wujiuye/asmbytecode/book/fourth/ClassLoaderMain.java
@@ -5,8 +5,8 @@ import java.net.URLClassLoader;
 public class ClassLoaderMain {
 
     public static void main(String[] args) throws Exception {
-        loadClass1();
-//        loadClass2();
+        // loadClass1();
+        loadClass2();
     }
 
     public static void loadClass1() throws Exception {
@@ -18,8 +18,8 @@ public class ClassLoaderMain {
 
     public static void loadClass2() throws ClassNotFoundException {
         Class<?> classLoaderTestClass = Class.forName("com.wujiuye.asmbytecode.book.fourth.ClassLoaderTest");
-        System.out.println(classLoaderTestClass.getName());
-        System.out.println(classLoaderTestClass.getClassLoader().getClass().getName());
+        // System.out.println(classLoaderTestClass.getName());
+        // System.out.println(classLoaderTestClass.getClassLoader().getClass().getName());
     }
 
 }
